33dc71b6196449ece79bc504363594f5ae15a460
winclip <sokhotnikoff@gmail.com>
2025-09-19 16:33:35 +0200

    feat: add unique email validation and hash user password

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,20 +2,35 @@
 
 import { IFormData } from "@/types/form-data";
 import prisma from "@/utils/prisma";
+import bcrypt from "bcrypt";
 
 export async function registerUser(data: IFormData) {
   try {
     const { email, password, confirmPassword } = data;
 
+    if (password !== confirmPassword) {
+      return { error: "Passwords do not match" };
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (existingUser) {
+      return { error: "User already exists" };
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await prisma.user.create({
       data: {
-        email: email,
-        password: password,
+        email,
+        password: hashedPassword,
       },
     });
-    return user;
+
+    return { success: true, user };
   } catch (error) {
-    console.log(error);
-    return { error: "Error while register" };
+    console.error(error);
+    return { error: "Error while registering user" };
   }
 }
