66f059bd17b20e56b2885c010886a7fd8818481e
winclip <sokhotnikoff@gmail.com>
2025-09-24 22:08:33 +0200

    feat: add session-aware auth buttons with login, register, and sign out

--- src/components/UI/layout/AuthButtons.tsx
+++ src/components/UI/layout/AuthButtons.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { Button, NavbarItem } from "@heroui/react";
-import Link from "next/link";
 import { useState } from "react";
+import { useSession } from "next-auth/react";
 import RegistrationModal from "../modals/Registration.modal";
 import LoginModal from "../modals/Login.modal";
 import { signOutFunc } from "@/actions/sign-out";
@@ -11,35 +11,53 @@ export default function AuthButtons() {
   const [isRegistrationOpen, setIsRegistrationOpen] = useState(false);
   const [isLoginOpen, setIsLoginOpen] = useState(false);
 
+  const { data: session, status } = useSession();
+  const isAuth = !!session;
+
   const handleSignOut = async () => {
     await signOutFunc();
   };
+
+  if (status === "loading") {
+    return <NavbarItem>Loading...</NavbarItem>;
+  }
+
   return (
     <>
-      <NavbarItem>
-        <Button
-          color="secondary"
-          variant="flat"
-          onPress={() => setIsLoginOpen(true)}
-        >
-          Login
-        </Button>
-      </NavbarItem>
-      <NavbarItem>
-        <Button
-          color="primary"
-          variant="flat"
-          onPress={() => setIsRegistrationOpen(true)}
-        >
-          Register
-        </Button>
-      </NavbarItem>
+      {!isAuth ? (
+        <>
+          <NavbarItem>
+            <Button
+              color="secondary"
+              variant="flat"
+              onPress={() => setIsLoginOpen(true)}
+            >
+              Login
+            </Button>
+          </NavbarItem>
+          <NavbarItem>
+            <Button
+              color="primary"
+              variant="flat"
+              onPress={() => setIsRegistrationOpen(true)}
+            >
+              Register
+            </Button>
+          </NavbarItem>
+        </>
+      ) : (
+        <>
+          <NavbarItem>
+            <Button color="primary" variant="flat" onPress={handleSignOut}>
+              Sign out
+            </Button>
+          </NavbarItem>
+          <NavbarItem>
+            Zdravo {session.user?.name || session.user?.email}!
+          </NavbarItem>
+        </>
+      )}
 
-      <NavbarItem>
-        <Button color="primary" variant="flat" onPress={handleSignOut}>
-          Sign out
-        </Button>
-      </NavbarItem>
       <RegistrationModal
         isOpen={isRegistrationOpen}
         onClose={() => setIsRegistrationOpen(false)}
